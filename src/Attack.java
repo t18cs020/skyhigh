c571d72672983905ee296cb0bbf82c3da2011c12
t18cs020 <72594537+t18cs020@users.noreply.github.com>
2022-11-05 17:14:25 +0900

    Update Attack.java

--- src/Attack.java
+++ src/Attack.java
@@ -1,10 +1,10 @@
 
-public class Attack {
+public class Attack {//プレイヤーの弾のクラス
 
-	private int atx;
-	private int aty;
-	private boolean exist;
-	private boolean through;
+	private int atx;//x座標
+	private int aty;//y座標
+	private boolean exist;//弾があるかどうか
+	private boolean through;//衝突したかどうか
 	private static final int WIDTH = Game.WIN_WIDTH;
 	private static final int HEIGHT = Game.WIN_HEIGHT;
 	public static final int SIZE = 40;
@@ -16,23 +16,23 @@ public class Attack {
 		this.exist = false;
 		this.through = false;
 	}
-	
+	/*画面外に出た時の処理*/
 	public void isOutOfScreen() {
 		if(atx <= 0 || atx > WIDTH || aty < 0 || aty > HEIGHT)
 			exist = false;
 	}
-	
+	/*撃った時の処理*/
 	public void shotAttack(int apx, int apy) {
 		exist = true;
 		through = false;
 		atx = apx;
 		aty = apy;
 	}
-	
+	/*更新*/
 	public void updateAttack(int speed) {
 		atx += speed;
 	}
-	
+	/*弾が当たった時の処理*/
 	public void reach() {
 		exist = false;
 		through = true;
