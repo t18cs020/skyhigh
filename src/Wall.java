fb3ead52469778d8448d7e402f4e79339b05daa2
t18cs020 <72594537+t18cs020@users.noreply.github.com>
2022-11-05 17:43:06 +0900

    Update Wall.java

--- src/Wall.java
+++ src/Wall.java
@@ -1,11 +1,11 @@
 
-public class Wall {
-	private int wx;
-	private int wy;
-	private boolean exist;
-	private boolean through;
-	public static final int HEIGHT = 40;
-	private static final int[] SPEED = {5,5,7};
+public class Wall {//壁（障害物）のクラス
+	private int wx;//x座標
+	private int wy;//y座標
+	private boolean exist;//存在するかどうか
+	private boolean through;//衝突したか
+	public static final int HEIGHT = 40;//サイズ
+	private static final int[] SPEED = {5,5,7};//速さ
 	
 	public Wall(int wy) {
 		super();
@@ -14,14 +14,14 @@ public class Wall {
 		this.exist = true;
 		this.through = false;
 	}
-	
+	/*更新*/
 	public void updateWall(int level) {
 		wx -= SPEED[level];
 		if(wx < 0) 
 			exist = false;
 	}
 
-	
+	/*弾が当たった時の処理*/
 	public boolean hitWall(int atx) {
 		if(atx >= wx) {
 			exist = false;
